74a6390cef87e84448d0141ad2e561865ae91894
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-05-07 20:02:53 +0600

    🔧 update(config): updated configs

--- backend/src/app/modules/user/user.types.ts
+++ backend/src/app/modules/user/user.types.ts
@@ -1,8 +1,8 @@
-import type { Document, Model, Types } from 'mongoose';
-import type { z } from 'zod';
-import type { authValidations } from '@/modules/auth/auth.validation';
-import { userValidations } from '@/modules/user/user.validation';
-import type { TEmail } from '@/types';
+import type { Document, Model, Types } from "mongoose";
+import type { z } from "zod";
+import type { authValidations } from "@/modules/auth/auth.validation";
+import type { userValidations } from "@/modules/user/user.validation";
+import type { TEmail } from "@/types";
 
 export type TUser = z.infer<typeof userValidations.creationSchema>;
 
@@ -30,7 +30,7 @@ export interface IUserModel extends Model<IUserDoc> {
 	validateUser(email?: TEmail): Promise<IUserDoc>;
 }
 
-export interface ICurrentUser extends Omit<TUser, 'password'> {
+export interface ICurrentUser extends Omit<TUser, "password"> {
 	_id: Types.ObjectId;
 	created_at: string;
 	updated_at: string;
--- backend/src/app/types/index.ts
+++ backend/src/app/types/index.ts
@@ -1,21 +1,28 @@
-import type { Types } from 'mongoose';
-import type { HttpStatusCode, StatusCode } from 'nhb-toolbox/http-status/types';
-import type { COLLECTIONS, USER_ROLES } from '@/constants';
-import type { TLoginCredentials } from '@/modules/user/user.types';
+import type { Types } from "mongoose";
+import type { HttpStatusCode, StatusCode } from "nhb-toolbox/http-status/types";
+import type { COLLECTIONS, USER_ROLES } from "@/constants";
+import type { TLoginCredentials } from "@/modules/user/user.types";
 
 export type ExceptionSignal = NodeJS.UncaughtExceptionOrigin | NodeJS.Signals;
 
 export type TCollection = (typeof COLLECTIONS)[number];
 
-export type TMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS' | 'OK';
+export type TMethod =
+	| "GET"
+	| "POST"
+	| "PUT"
+	| "DELETE"
+	| "PATCH"
+	| "OPTIONS"
+	| "OK";
 
 export type TResponseDetails = { message: string; statusCode: StatusCode };
 
-export type TStatusCode = HttpStatusCode<'clientError' | 'serverError'>;
+export type TStatusCode = HttpStatusCode<"clientError" | "serverError">;
 
 export type TUserRole = (typeof USER_ROLES)[number];
 
-export type TEmail = TLoginCredentials['email'];
+export type TEmail = TLoginCredentials["email"];
 
 export type SearchField<T> = {
 	[K in keyof T]: T[K] extends string | number ? K : never;
@@ -25,12 +32,15 @@ export type NumericKeys<T> = {
 	[K in keyof T]: T[K] extends number ? K : never;
 }[keyof T];
 
-export type ExcludeField<T> = `-${Extract<ExcludeVirtuals<FilterKeys<T>>, string>}`;
+export type ExcludeField<T> =
+	`-${Extract<ExcludeVirtuals<FilterKeys<T>>, string>}`;
 
 /** * Utility type to extract keys from `T` where the value is `string`, `number`, `boolean`, `Date` or `ObjectId`. */
 type FilterKeys<T> = {
-	[K in keyof T]: T[K] extends string | number | boolean | Date | Types.ObjectId ? K : never;
+	[K in keyof T]: T[K] extends string | number | boolean | Date | Types.ObjectId
+		? K
+		: never;
 }[keyof T];
 
 /** * Utility type to exclude Mongoose virtual properties (e.g., isNew). */
-type ExcludeVirtuals<T> = Exclude<T, 'isNew' | 'id'>;
+type ExcludeVirtuals<T> = Exclude<T, "isNew" | "id">;
