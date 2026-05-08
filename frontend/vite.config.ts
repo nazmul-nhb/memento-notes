55d02bae1ba604491f7c44ac857a434d449281ec
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-05-08 14:14:29 +0600

    🔧 update(frontend): installed required packages to build the ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+import path from 'node:path';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
@@ -6,11 +6,11 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
-    server: { host: true },
-    resolve: {
-        alias: {
-            '@/': fileURLToPath(new URL('./src/', import.meta.url)),
-        },
-    },
+	plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+	server: { host: true },
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
